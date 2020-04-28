93963bf4c6deec9de7646e8bc85abcbfb515cfcf
Gabrysia <gabin301011@gmail.com>
2020-04-28 19:45:23 +0200

    lab 7 done

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { SearchBarComponent } from './shared/search-bar/search-bar.component';
 import { BlogHomeComponent } from './components/blog-home/blog-home.component';
 import { TextFormatDirective } from './directives/text-format.directive';
 import { FilterPipe } from './pipes/filter.pipe';
+import { SelectizeComponent } from './components/selectize/selectize.component';
 
 @NgModule({
   declarations: [
@@ -36,6 +37,7 @@ import { FilterPipe } from './pipes/filter.pipe';
     BlogHomeComponent,
     TextFormatDirective,
     FilterPipe,
+    SelectizeComponent,
   ],
   imports: [
     BrowserModule,
